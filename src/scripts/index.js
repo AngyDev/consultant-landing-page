0c00658be571f76adc657ed2d2e6cf4346f113c8
Angela Busato <busato.angela@gmail.com>
2021-04-09 10:42:25 +0200

    [remove] comments code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,23 +4,3 @@ import './accordion';
 import './slideshow';
 import './form';
 import './validation';
-
-/*async function getData() {
-    try {
-        //const response = await fetch("https://api.mocki.io/v1/068c5d2e");
-        const response = await fetch("https://e3c711b4-41bb-4b0b-b1b5-944550274dc9.mock.pstmn.io/v1/home");
-        console.log(response);
-        if (response.status != 200) {
-            var responseError = 'Something is wrong! Status Code: ' + response.status;
-        }
-        console.log(responseError);
-
-        const data = await response.json();
-        console.log(data);
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-getData();*/
