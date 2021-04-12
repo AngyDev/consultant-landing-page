b6fb1c34f9779be35a6ec3ae6d4952f24ffd404a
Angela Busato <busato.angela@gmail.com>
2021-04-12 15:15:19 +0200

    [add] validation js test

---
+++ src/scripts/validation.spec.js
@@ -0,0 +1,34 @@
+import Validation from './validation';
+
+describe("Validation of input login form", () => {
+    it("Input is not valid", () => {
+
+        document.body.innerHTML = `
+            <input id="name" class="form__input" type="text" placeholder="Your Name" value="">
+            <span id="error-name" class="form__font error"></span>`;
+
+        const validation = new Validation();
+
+        const nameField = document.getElementById("name");
+        const errorName = document.getElementById("error-name");
+        const valid = validation.inputValidation(nameField, errorName);
+
+        expect(valid).toBeFalsy();
+    });
+
+    it("Input is valid", () => {
+
+        document.body.innerHTML = `
+            <input id="name" class="form__input" type="text" placeholder="Your Name" value="">
+            <span id="error-name" class="form__font error"></span>`;
+
+        const validation = new Validation();
+
+        const nameField = document.getElementById("name");
+        nameField.value = "Angela";
+        const errorName = document.getElementById("error-name");
+        const valid = validation.inputValidation(nameField, errorName);
+
+        expect(valid).toBeTruthy();
+    })
+})
