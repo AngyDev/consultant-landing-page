c90df722166c68026b901df18259ab3cb16765a9
Angela Busato <busato.angela@gmail.com>
2021-04-14 12:23:47 +0200

    [add] login button action

---
+++ src/scripts/loginbutton.js
@@ -0,0 +1,12 @@
+import Login from './login';
+
+const nameField = document.getElementById("name");
+const password = document.getElementById("password");
+const signIn = document.getElementById("signIn");
+const error = document.querySelectorAll('.error');
+
+const login = new Login();
+
+signIn && signIn.addEventListener('click', () => {
+    login.checkInputAndLogin(nameField, password, error);
+});
