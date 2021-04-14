6dde44fad6b895b2c638cf1457aaf01031839f59
Angela Busato <busato.angela@gmail.com>
2021-04-14 09:46:10 +0200

    [fix] class for menu burger button

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,15 +1,29 @@
+class Menu {
+    constructor() {}
+
+    /**
+     * Show and hide the list of links of the menu
+     * @param {HTMLElement} links The list of links of the menu
+     * @param {HTMLElement} btnContact The menu button
+     */
+    menuDropdown(links, btnContact) {
+        // Change the content of the li element
+        btnContact.innerHTML = "<a>Contact</a>";
+
+        if (links.style.display === "block") {
+            links.style.display = "none";
+        } else {
+            links.style.display = "block";
+        }
+    }
+}
+
 const btnBurger = document.getElementById("btn-burger");
+const btnContact = document.getElementById("btn-contact");
+const links = document.getElementById("myLinks");
+
+const menu = new Menu();
 
 btnBurger.addEventListener('click', () => {
-    // Change the content of the li element
-    const btnContact = document.getElementById("btn-contact");
-    btnContact.innerHTML = "<a>Contact</a>";
-
-    const links = document.getElementById("myLinks");
-
-    if (links.style.display === "block") {
-        links.style.display = "none";
-    } else {
-        links.style.display = "block";
-    }
+    menu.menuDropdown(links, btnContact);
 })
