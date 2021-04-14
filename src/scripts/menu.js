dde68df72a93968e6af62977b001dd806078e55d
Angela Busato <busato.angela@gmail.com>
2021-04-14 10:07:22 +0200

    [fix] change the class for the burger menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,26 +4,18 @@ class Menu {
     /**
      * Show and hide the list of links of the menu
      * @param {HTMLElement} links The list of links of the menu
-     * @param {HTMLElement} btnContact The menu button
      */
-    menuDropdown(links, btnContact) {
-        // Change the content of the li element
-        btnContact.innerHTML = "<a>Contact</a>";
-
-        if (links.style.display === "block") {
-            links.style.display = "none";
-        } else {
-            links.style.display = "block";
-        }
+    menuDropdown(links) {
+        // adds and removes the class to display or not the menu in mobile or desktop mode
+        links.classList.toggle("active-menu");
     }
 }
 
 const btnBurger = document.getElementById("btn-burger");
-const btnContact = document.getElementById("btn-contact");
 const links = document.getElementById("myLinks");
 
 const menu = new Menu();
 
 btnBurger.addEventListener('click', () => {
-    menu.menuDropdown(links, btnContact);
+    menu.menuDropdown(links);
 })
